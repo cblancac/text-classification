3327f8eab0133f7e81a8b0489c1b1ce187e5f3f8
cblancac <carblacac7@gmail.com>
2023-04-04 19:10:32 +0200

    create data loader

---
+++ dataset/load_data.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from datasets import load_dataset
+from datasets.dataset_dict import DatasetDict
+from datasets.arrow_dataset import Dataset
+
+
+class LoadDataset():
+    def __init__(self, dataset_name: str):
+        self.dataset_name = dataset_name
+
+    def load_data(self) -> DatasetDict:
+        return load_dataset(self.dataset_name)      
+    
+    def split_train_val_test(self, data: DatasetDict) -> List[Dataset]:
+        train       = data['train']
+        validation  = data['validation']
+        test        = data['test']
+        return train, validation, test
+    
+
+
+if __name__ == '__main__':
+    data_loader = LoadDataset('emotion')
+    data = data_loader.load_data()
+    train, validation, test = data_loader.split_train_val_test(data)
+
+    print('end')
