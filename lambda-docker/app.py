ccc7d5775d9f143741803d80cb949c51547f10a4
EC2 Default User <ec2-user@ip-172-31-28-2.eu-west-1.compute.internal>
2023-05-15 13:38:38 +0000

    create docker lambda

--- lambda-docker/app.py
+++ lambda-docker/app.py
@@ -8,10 +8,11 @@ from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 
 from exceptions import BadContentType, EndpointError, BadRequest
-
+import os
+os.environ['TRANSFORMERS_CACHE'] = "./distilbert-base-uncased-finetuned-emotion"
 
 # Specify the path of the model
-model_ckpt = Path("/opt/ml/model")
+model_ckpt = Path("./distilbert-base-uncased-finetuned-emotion")
 
 # Load the fine-tuned tokenizer and model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -19,30 +20,11 @@ tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
 model = AutoModelForSequenceClassification.from_pretrained(model_ckpt).to(device)
 
 
-def _get_json(request_body):
-    print(type(request_body))
-    try:
-        decoded_request = request_body.decode("utf8").replace("'", '"')
-    except AttributeError:
-        decoded_request = request_body.replace("'", '"')
-    try:
-        query_value = re.search("(?<={\s*\"query\":\s*\")(.)+(?=\s*\"\s*}$)", decoded_request).group(0)
-        query_value_fixed = query_value.replace('"', "")
-        decoded_request_fixed = decoded_request.replace(query_value, query_value_fixed)
-        print("-------------------- REQUEST BODY --------------------")
-        print(f"TYPE of decoded request is: {type(decoded_request)}")
-        print(f"CONTENT of decoded request is: {decoded_request}")
-        print("------------------------------------------------------")
-        json_request = json.loads(decoded_request_fixed, strict=False)
-        return json_request
-    except:
-        return BadRequest()
-        
-
 def inference(text: str) -> str:
     """This function take a text as an input and
        predict whether or not it is a header. The input
        has to be line extracted by tesseract"""
+    print("TTTTTTTTTTTTT: ", text)
     inputs = tokenizer(text, return_tensors="pt")
 
     inputs = {k:v.to(device) for k,v in inputs.items()}
@@ -53,26 +35,23 @@ def inference(text: str) -> str:
     max_vale = max(predictions)
     idx = predictions.index(max_vale)
     return model.config.id2label[idx]
-    
-    
-def get_text(request_body, request_content_type):
+
+
+def get_text(event, context):
     # Get the text sent by Postman
-    if request_content_type =="application/json":
-        decoded_payload = _get_json(request_body)
-        text = decoded_payload["query"]
-        return text
-        
-    else:
-        return BadContentType(["application/json"])
-        
-       
-def handler(request_body, request_content_type):
-    text = get_text(request_body, request_content_type)
+    raw_string = r'{}'.format(event['body'])
+    body = json.loads(raw_string)
+    original_text = body['text']
+    return original_text
+
+
+def handler(event, context):
+    text = get_text(event, context)
     emotion_detected = inference(text)
+    final = {'output': emotion_detected}
+
     return {
-        'headers': {'Content-Type' : 'application/json'},
         'statusCode': 200,
-        'body': json.dumps({"message": "Lambda Container image invoked!",
-                            "event": emotion_detected})
+        'body': json.dumps(final)
     }
 
